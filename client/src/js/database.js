64d65d321d7a09a30ec45ca1fd558d4519f876fb
Mike Kehoe <mkehoe2996@gmail.com>
2023-05-23 16:01:18 -0400

    first commit

---
+++ client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put(content);
+  await tx.complete;
+  console.log('🚀 - data saved to the database', result);
+
+}
+
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.complete;
+  console.log('🚀 - data retrieved from the database', content);
+  return content;
+}
+
+initdb();
